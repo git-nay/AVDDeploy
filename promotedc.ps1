02d9bdad2bd83e550bca610c3b6be572482b03dd
Nathan <89515864+git-nay@users.noreply.github.com>
2026-06-12 15:01:29 +0100

    Update promotedc.ps1

--- promotedc.ps1
+++ promotedc.ps1
@@ -1,1 +1,10 @@
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 
+$Password = ConvertTo-SecureString 'P@ssw0rd123!' -AsPlainText -Force
+
+Install-ADDSForest `
+    -DomainName 'nathlabs.local' `
+    -DomainNetbiosName 'NATH' `
+    -InstallDNS `
+    -SafeModeAdministratorPassword $Password `
+    -Force
